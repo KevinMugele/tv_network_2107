ffefa6d21c22191b8da4e93e04a415edcb5f2d36
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-21 11:24:15 -0400

    Finish iteration 2. Write tests and methods for total salary, highest paid actor, and actors

--- lib/show.rb
+++ lib/show.rb
@@ -8,5 +8,25 @@ class Show
     @characters = characters
   end
 
+  def total_salary
+    salaries = 0
+    @characters.each do |character|
+      salaries += character.salary
+    end
+    salaries
+  end
 
+  def highest_paid_actor
+    highest_paid = @characters.max_by do |character|
+      character.salary
+    end
+    highest_paid.actor
+  end
+
+  def actors
+    actors_list = @characters.map do |character|
+      character.actor
+    end
+    actors_list
+  end 
 end
--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -37,4 +37,32 @@ RSpec.describe Show do
 
     expect(knight_rider.characters).to eq([michael_knight, kitt])
   end
+
+  it "calculates total salary" do
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+
+    expect(knight_rider.total_salary).to eq(2600000)
+  end
+
+  it "finds higest paid actor" do
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+
+    expect(knight_rider.highest_paid_actor).to eq("David Hasselhoff")
+  end
+
+
+  it "lists all the actors" do
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+
+    expect(knight_rider.actors).to eq(["David Hasselhoff", "William Daniels"])
+  end
 end
