e4ca99586de882d01d873b72ca98094624e220ad
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-21 11:47:57 -0400

    Complete iteration 3. Finish tests and methods for main characters and actors by show

--- lib/network.rb
+++ lib/network.rb
@@ -9,6 +9,21 @@ class Network
 
   def add_show(show)
     @shows << show
-  end   
+  end
 
+  def main_characters
+    all_characters = []
+    @shows.each do |show|
+      all_characters << show.actors
+    end
+    all_characters.flatten
+  end
+
+  def actors_by_show
+    actors_by_show_hash = {}
+    @shows.each do |show, actor|
+      actors_by_show_hash[show] = show.actors
+    end
+    actors_by_show_hash
+  end
 end
