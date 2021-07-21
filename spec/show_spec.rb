aadef717a240c8276004970b18a8a3a47206f710
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-21 11:09:44 -0400

    Write initial tests and initialize variables for Show class

---
+++ spec/show_spec.rb
@@ -0,0 +1,13 @@
+require './lib/character'
+require './lib/show'
+
+RSpec.describe Show do
+  it "exists" do
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+
+    expect(knight_rider).to be_a(Show)
+  end
+end 
