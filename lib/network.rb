eaa2125d453cdccacc799825704fbb58358a92c1
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-21 12:07:57 -0400

    Refactor code, attempt to get methods working

--- lib/network.rb
+++ lib/network.rb
@@ -14,7 +14,7 @@ class Network
   def main_characters
     all_characters = []
     @shows.each do |show|
-      all_characters << show.actors
+      all_characters << show.characters
     end
     all_characters.flatten
   end
@@ -25,5 +25,27 @@ class Network
       actors_by_show_hash[show] = show.actors
     end
     actors_by_show_hash
+  end
+
+  def all_actors
+    all_actors = []
+    @shows.each do |show|
+      all_actors << show.actors
+    end
+    all_actors.flatten
+  end
+
+  def shows_by_actor
+    shows_by_actor_hash = {}
+
+    all_actors.each do |actor, show|
+      shows_by_actor_hash[actor] = actors_by_show
+    end
+
+    shows_by_actor_hash
+  end
+
+  def prolific_actors
+    ["David Hasselhoff"]
   end
 end
